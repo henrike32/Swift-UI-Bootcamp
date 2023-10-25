ebad62da22b2aad43036781adc657206e523e33f
Henrique Lambertucci <henrique.lambertucci@gmail.com>
2023-10-25 12:10:37 -0300

    update 2

--- SwiftfulThinkingBootcamp/BackgroundAndOverlayBootcamp.swift
+++ SwiftfulThinkingBootcamp/BackgroundAndOverlayBootcamp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, my World!!!")
             .background(
                 Circle()
                     .fill(.blue)
